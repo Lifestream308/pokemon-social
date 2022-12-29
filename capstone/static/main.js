17c190e4e456b116fa6ef88b0e1b0614020a0c0a
Lifestream308 <99936729+Lifestream308@users.noreply.github.com>
2022-12-29 00:27:04 -0800

    Add files via upload

--- capstone/static/main.js
+++ capstone/static/main.js
@@ -10,53 +10,67 @@ async function getText() {
     console.log(obj.id)
     console.log(obj.names[8].name)
     console.log("The " + obj.genera[7].genus)
-    console.log(obj.flavor_text_entries[1].flavor_text)
+    console.log(obj.flavor_text_entries[1].flavor_text.replaceAll("\n", " "))
   }
 
-function chooseImages() {
+async function chooseImages() {
     input = document.getElementById('1stinput')
     image = document.getElementById('1stimage')
-    image.src = '/static/images/' + input.value + '.gif'
-    title1 = document.getElementById('title1')
-    title1.innerHTML = obj[input.value - 1].title
-    content1 = document.getElementById('content1')
-    content1.innerHTML = obj[input.value - 1].content
-    number1 = document.getElementById('number1')
-    number1.innerHTML = '# ' + obj[input.value - 1].number
-    type1 = document.getElementById('type1')
-    type1.innerHTML = 'Type - ' + obj[input.value - 1].type
-    weakness1 = document.getElementById('weakness1')
-    weakness1.innerHTML = 'Weaknesses - ' + obj[input.value - 1].weakness
-
+    if (input.value > 0) {
+      let x = await fetch(`https://pokeapi.co/api/v2/pokemon-species/${input.value}/`);
+      let y = await x.text();
+      obj = JSON.parse(y)
+      image.src = '/static/images/' + input.value + '.gif'
+      title1 = document.getElementById('title1')
+      title1.innerHTML = obj.names[8].name
+      content1 = document.getElementById('content1')
+      content1.innerHTML = obj.flavor_text_entries[1].flavor_text.replaceAll("\n", " ").replaceAll("\f", " ")
+      number1 = document.getElementById('number1')
+      number1.innerHTML = '# ' + obj.id + " The " + obj.genera[7].genus
+      // type1 = document.getElementById('type1')
+      // type1.innerHTML = 'Type - ' + obj.genera[7].genus
+      // weakness1 = document.getElementById('weakness1')
+      // weakness1.innerHTML = 'Weaknesses - ' + obj[input.value - 1].weakness
+    }
 
     input = document.getElementById('2ndinput')
     image = document.getElementById('2ndimage')
-    image.src = '/static/images/' + input.value + '.gif'
-    title2 = document.getElementById('title2')
-    title2.innerHTML = obj[input.value - 1].title
-    content2 = document.getElementById('content2')
-    content2.innerHTML = obj[input.value - 1].content
-    number2 = document.getElementById('number2')
-    number2.innerHTML = '# ' + obj[input.value - 1].number
-    type2 = document.getElementById('type2')
-    type2.innerHTML = 'Type - ' + obj[input.value - 1].type
-    weakness2 = document.getElementById('weakness2')
-    weakness2.innerHTML = 'Weaknesses - ' + obj[input.value - 1].weakness
+    if (input.value > 0) {
+      let x = await fetch(`https://pokeapi.co/api/v2/pokemon-species/${input.value}/`);
+      let y = await x.text();
+      obj = JSON.parse(y)
+      image.src = '/static/images/' + input.value + '.gif'
+      title2 = document.getElementById('title2')
+      title2.innerHTML = obj.names[8].name
+      content2 = document.getElementById('content2')
+      content2.innerHTML = obj.flavor_text_entries[1].flavor_text.replaceAll("\n", " ").replaceAll("\f", " ")
+      number2 = document.getElementById('number2')
+      number2.innerHTML = '# ' + obj.id + " The " + obj.genera[7].genus
+    // type2 = document.getElementById('type2')
+    // type2.innerHTML = 'Type - ' + obj[input.value - 1].type
+    // weakness2 = document.getElementById('weakness2')
+    // weakness2.innerHTML = 'Weaknesses - ' + obj[input.value - 1].weakness
+    }
 
 
     input = document.getElementById('3rdinput')
     image = document.getElementById('3rdimage')
-    image.src = '/static/images/' + input.value + '.gif'
-    title3 = document.getElementById('title3')
-    title3.innerHTML = obj[input.value - 1].title
-    content3 = document.getElementById('content3')
-    content3.innerHTML = obj[input.value - 1].content
-    number3 = document.getElementById('number3')
-    number3.innerHTML = '# ' + obj[input.value - 1].number
-    type3 = document.getElementById('type3')
-    type3.innerHTML = 'Type - ' + obj[input.value - 1].type
-    weakness3 = document.getElementById('weakness3')
-    weakness3.innerHTML = 'Weaknesses - ' + obj[input.value - 1].weakness
+    if (input.value > 0) {
+      let x = await fetch(`https://pokeapi.co/api/v2/pokemon-species/${input.value}/`);
+      let y = await x.text();
+      obj = JSON.parse(y)
+      image.src = '/static/images/' + input.value + '.gif'
+      title3 = document.getElementById('title3')
+      title3.innerHTML = obj.names[8].name
+      content3 = document.getElementById('content3')
+      content3.innerHTML = obj.flavor_text_entries[1].flavor_text.replaceAll("\n", " ").replaceAll("\f", " ")
+      number3 = document.getElementById('number3')
+      number3.innerHTML = '# ' + obj.id + " The " + obj.genera[7].genus
+    // type3 = document.getElementById('type3')
+    // type3.innerHTML = 'Type - ' + obj[input.value - 1].type
+    // weakness3 = document.getElementById('weakness3')
+    // weakness3.innerHTML = 'Weaknesses - ' + obj[input.value - 1].weakness
+    }
 }
 
 function hitEnter() {
